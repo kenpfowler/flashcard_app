9a518e1fd58aec7479e1d0c545b0bf4c3a83bdd5
Ken Fowler <development@kennyf.ca>
2023-11-02 20:39:13 -0400

    feat: scaffold pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+  const subjects = await prisma.subject.findMany();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <ul>
+        {subjects.map((subject) => (
+          <li key={subject.id}>{subject.title}</li>
+        ))}
+      </ul>
       <Button>Click me</Button>
     </main>
   );
