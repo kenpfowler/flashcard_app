843df68c2399e52db05f1d73a1df92592b704139
Ken Fowler <development@kennyf.ca>
2023-11-03 10:59:55 -0400

    feat: create basic page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,25 @@
-import { Button } from "@/components/ui/button";
 import prisma from "@/lib/prisma";
 
 export default async function Home() {
-  const subjects = await prisma.subject.findMany();
+  const subjects = await prisma.subject.findMany({
+    include: { decks: { include: { cards: { include: { answers: true } } } } },
+  });
+
+  const hasSubjects = subjects.length > 0;
+
+  // TODO:
+  // each subject should be an clickable accordion that reveals associated decks
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ul>
-        {subjects.map((subject) => (
-          <li key={subject.id}>{subject.title}</li>
-        ))}
-      </ul>
-      <Button>Click me</Button>
+    <main className="min-h-screen flex flex-col items-center justify-between p-24">
+      <div className="flex flex-col items-center w-full">
+        <h1 className="text-center">Hello, World!</h1>
+        {hasSubjects ? (
+          subjects.map((subject) => <h2 key={subject.id}>{subject.title}</h2>)
+        ) : (
+          <h2>You haven't created any subjects yet</h2>
+        )}
+      </div>
     </main>
   );
 }
