8786cf6118c2778c7a9c29ad897d3d93ce3be8bd
Ken Fowler <development@kennyf.ca>
2023-12-11 00:15:37 -0500

    feat: add session page

---
+++ src/app/session/FlashCard.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { toast } from "@/components/ui/use-toast";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Answer, Card as PrismaCard } from "@prisma/client";
+
+type FlashCardProps = {
+  item: PrismaCard & { answers: Array<Answer> };
+};
+
+export function FlashCard({ item }: FlashCardProps) {
+  const answers = item.answers.map((answer) => answer);
+  const init: [string, ...string[]] = ["init"];
+  const ids = answers.map((answer) => {
+    return answer.id.toString();
+  });
+
+  for (let index = 0; index < ids.length; index++) {
+    init[index] = ids[index];
+  }
+
+  if (!ids.length) {
+    return;
+  }
+
+  const FormSchema = z.object({
+    selection: z.enum(init, {
+      required_error: "You need to select an answer.",
+    }),
+  });
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
+  return (
+    <Card className="w-[350px]" key={item.id}>
+      <CardHeader>
+        <CardTitle>{item.question}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-2/3 space-y-6"
+          >
+            <FormField
+              control={form.control}
+              name="selection"
+              render={({ field }) => (
+                <FormItem className="space-y-3">
+                  <FormLabel>Select an answer</FormLabel>
+                  <FormControl>
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      className="flex flex-col space-y-1"
+                    >
+                      {answers.map((answer) => (
+                        <FormItem
+                          key={answer.id}
+                          className="flex items-center space-x-3 space-y-0"
+                        >
+                          <FormControl>
+                            <RadioGroupItem value={answer.id.toString()} />
+                          </FormControl>
+                          <FormLabel className="font-normal">
+                            {answer.answerText}
+                          </FormLabel>
+                        </FormItem>
+                      ))}
+                    </RadioGroup>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ src/app/session/page.tsx
@@ -0,0 +1,32 @@
+import prisma from "@/lib/prisma";
+
+import { FlashCard } from "./FlashCard";
+
+export default async function SessionPage() {
+  // to start a quiz the user needs to select it from the deck
+
+  // subject
+  //  deck
+  // quiz <--- selecting the quiz from the hierarchy should trigger a fetch for all the data to run the quiz.
+
+  // to run through a quiz with a user we need display each question and answer combination
+  // the user needs to be able to select an answer and get feedback about the correctness
+  // the users responses need to be recorded for analysis
+
+  const items = await prisma.card.findMany({
+    where: { deckId: 2 },
+    include: { answers: true },
+  });
+
+  // each item has the answers for a specific question
+  // when we output an item we want to output the answers as well
+  // so, we get the items answer property and map over it
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      {items.map((item) => (
+        <FlashCard key={item.id} item={item} />
+      ))}
+    </div>
+  );
+}
