ca1d88f4d0021399c134492c5cb41ab9d62d5d16
Ken Fowler <development@kennyf.ca>
2023-12-18 13:28:30 -0500

    fix: default ui for subjects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
 import Link from "next/link";
 import { SelectSessionForm } from "./SelectSessionForm";
 import prisma from "@/lib/prisma";
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
   const subjects = await prisma.subject.findMany({
     include: { children: true },
   });
+
+  if (subjects.length === 0) {
+    return (
+      <main className="min-h-screen flex flex-col items-center justify-between p-24">
+        <div className="flex flex-col items-center space-y-2 justify-center ">
+          <h3>You haven't created and subjects to study.</h3>
+          <Button asChild>
+            <Link href={"/subjects/create"}>Create</Link>
+          </Button>
+        </div>
+      </main>
+    );
+  }
 
   return (
     <main className="min-h-screen flex flex-col items-center justify-between p-24">
