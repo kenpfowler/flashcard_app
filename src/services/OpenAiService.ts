133d28c37c4decf474c45a603ec84a60425d6796
Ken Fowler <development@kennyf.ca>
2024-01-15 17:44:44 -0500

    chore: deleted moved service

--- src/services/OpenAiService.ts
+++
@@ -1,9 +0,0 @@
-export interface IOpenAiApi {
-  parseQuestions: (question: string) => Promise<string[]>;
-  parseCards: (
-    questions: string[],
-    subject: string
-  ) => Promise<
-    Array<{ name: string; answers: string[]; correctAnswerIndex: number }>
-  >;
-}
