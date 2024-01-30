57d730cfffc5dcb8ad05f01e6cc0c6f7d6606bb1
Ken Fowler <development@kennyf.ca>
2024-01-29 20:22:58 -0500

    feat: add home button

--- src/app/(dashboard)/dashboard/SideNavigation.tsx
+++ src/app/(dashboard)/dashboard/SideNavigation.tsx
@@ -1,16 +1,22 @@
 "use client";
 
-import { BarChart, Book, Settings, User } from "lucide-react";
+import { BarChart, Book, Home, Settings, User } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 import Link from "next/link";
-import { LogoutButton } from "@/app/LogoutButton";
 
 export const SideNavigation = () => {
   return (
-    <nav className="flex px-5 py-2.5 h-full">
+    <nav className="flex px-5 py-5 h-full">
       <ul className="flex flex-col justify-between items-center">
         <div className="flex flex-col space-y-2">
+          <li>
+            <Link href={"/"}>
+              <span className="flex">
+                <Home width={25} height={25} />
+              </span>
+            </Link>
+          </li>
           <li>
             <Link href={"/dashboard"}>
               <span className="flex">
--- src/app/LogoutButton.tsx
+++ src/app/LogoutButton.tsx
@@ -4,7 +4,7 @@ import { logout } from "./action";
 export async function LogoutButton() {
   return (
     <form action={logout}>
-      <Button type="submit" value="logout">
+      <Button className="w-full" type="submit" value="logout">
         Logout
       </Button>
     </form>
